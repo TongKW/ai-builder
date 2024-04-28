7c6886adcbe4a59da9167f42bd5b905bb7c21459
TongKW <petertongkawai@gmail.com>
2024-04-28 18:01:10 +0800

    fix: avoid same uuid cached on server components

--- sandbox-demo/web/app/page.tsx
+++ sandbox-demo/web/app/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default async function MainPage() {
-  // 1: generate workflow id
-  const workflowId = uuidv4().replace(/-/g, "");
+export default function MainPage() {
+  useEffect(() => {
+    // 1: generate workflow id
+    const workflowId = uuidv4().replace(/-/g, "");
 
-  // 2: redirect page
-  redirect(`/workflow/${workflowId}`);
+    // 2: redirect page
+    redirect(`/workflow/${workflowId}`);
+  }, []);
+
+  return <></>;
 }
